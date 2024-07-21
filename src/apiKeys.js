4286f8b4b9cf86ed5a82322d4a588265b178f12f
Slave Tanushev <slavetanusev@gmail.com>
2024-07-21 15:56:48 +0200

    finished app

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,4 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-    key: "{2afe2cd3dcd795626f0e54152f491e14}",
-    base: "https://api.openweathermap.org/data/2.5/",
-  };
+  key: "2afe2cd3dcd795626f0e54152f491e14",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
